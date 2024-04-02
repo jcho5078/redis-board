4e31b876bcd58ebc7fa50e129f07de9315fa7b46
jcho5078 <jcho5078@gmail.com>
2024-04-02 23:54:31 +0900

    add userService

---
+++ src/main/java/com/study/board/exception/DuplicateIdException.java
@@ -0,0 +1,9 @@
+package com.study.board.exception;
+
+public class DuplicateIdException extends RuntimeException {
+
+    public DuplicateIdException(String msg) {
+        super(msg);
+    }
+
+}
--- src/main/java/com/study/board/service/impl/UserServiceImpl.java
+++ src/main/java/com/study/board/service/impl/UserServiceImpl.java
@@ -1,18 +1,38 @@
 package com.study.board.service.impl;
 
 import com.study.board.dto.UserDTO;
+import com.study.board.exception.DuplicateIdException;
+import com.study.board.mapper.UserProfileMapper;
 import com.study.board.service.UserService;
+import com.study.board.util.SHA256Util;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @Log4j2
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserProfileMapper userProfileMapper;
 
     @Override
-    public void register(UserDTO userProfile) {
+    public void register(UserDTO userDTO) {
+        boolean duplIdResult = isDuplicatedId(userDTO.getUserId());
+        if (duplIdResult) {
+            throw new DuplicateIdException("중복된 아이디입니다.");
+        }
+        userDTO.setCreateTime(new Date());
+        userDTO.setPassword(SHA256Util.encryptSHA256(userDTO.getPassword()));
+        int insertCount = userProfileMapper.register(userDTO);
 
+        if (insertCount != 1) {
+            log.error("insertMember ERROR! {}", userDTO);
+            throw new RuntimeException(
+                    "insertUser ERROR! 회원가입 오류.\n" + "Params : " + userDTO);
+        }
     }
 
     @Override
